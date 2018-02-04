06e8054cfb895e8a698e3a18b9add3dc3c918eef
Shogun <shogun@cowtech.it>
2018-02-04 11:56:54 +0100

    Initial commit.

---
+++ README.md
@@ -0,0 +1,35 @@
+# tempera
+
+[![Release](https://img.shields.io/github/release/ShogunPanda/tempera.svg)](https://github.com/ShogunPanda/tempera/releases/latest)
+[![GoDoc](https://godoc.org/github.com/ShogunPanda/tempera?status.svg)](https://godoc.org/github.com/ShogunPanda/tempera)
+[![Go Report Card](https://goreportcard.com/badge/github.com/ShogunPanda/tempera)](https://goreportcard.com/report/github.com/ShogunPanda/tempera)
+[![Build Status](https://img.shields.io/travis/rust-lang/rust/master.svg)](https://travis-ci.org/ShogunPanda/tempera)
+[![License](https://img.shields.io/github/license/ShogunPanda/tempera.svg)](https://github.com/ShogunPanda/tempera/blob/master/LICENSE.md)
+
+Template based terminal coloring made really easy.
+
+http://sw.cowtech.it/tempera
+
+## Usage
+
+*TODO*
+
+## API Documentation
+
+The API documentation can be found [here](https://godoc.org/github.com/ShogunPanda/tempera).
+
+## Contributing to tempera
+
+* Check out the latest master to make sure the feature hasn't been implemented or the bug hasn't been fixed yet.
+* Check out the issue tracker to make sure someone already hasn't requested it and/or contributed it.
+* Fork the project.
+* Start a feature/bugfix branch.
+* Commit and push until you are happy with your contribution.
+* Make sure to add tests for it. This is important so I don't break it in a future version unintentionally.
+
+## Copyright
+
+Copyright (C) 2018 and above Shogun (shogun@cowtech.it).
+
+Licensed under the MIT license, which can be found at https://choosealicense.com/licenses/mit/.
+
---
+++ main_test.go
@@ -0,0 +1,103 @@
+/*
+ * This file is part of tempera. Copyright (C) 2018 and above Shogun <shogun@cowtech.it>.
+ * Licensed under the MIT license, which can be found at https://choosealicense.com/licenses/mit/.
+ */
+
+package tempera
+
+import (
+	"errors"
+	"testing"
+
+	. "github.com/franela/goblin"
+)
+
+func Test(t *testing.T) {
+	g := Goblin(t)
+
+	g.Describe("TestColorize", func() {
+		g.It("Applies known styles", func() {
+			g.Assert(Colorize("ABC", "bgBlack", "red")).Equal("\x1b[40m\x1b[31mABC\x1b[39m\x1b[49m")
+		})
+
+		g.It("Ignores known styles", func() {
+			g.Assert(Colorize("ABC", "whatever", "red")).Equal("\x1b[31mABC\x1b[39m")
+		})
+
+		g.It("Supports ANSI 256 colors, ignoring invalid colors", func() {
+			g.Assert(Colorize("ABC", "ANSI:232")).Equal("\x1b[38;5;232mABC\x1b[39m")
+			g.Assert(Colorize("ABC", "bgANSI:333")).Equal("ABC")
+			g.Assert(Colorize("ABC", "bgansi:2,4,0")).Equal("\x1b[48;5;112mABC\x1b[49m")
+			g.Assert(Colorize("ABC", "ANSI:2,4,6")).Equal("ABC")
+		})
+
+		g.It("Supports ANSI 16m RGB colors, ignoring invalid colors", func() {
+			g.Assert(Colorize("ABC", "rgb:255,232,0")).Equal("\x1b[38;2;255;232;0mABC\x1b[39m")
+			g.Assert(Colorize("ABC", "bgRGB:33,66,99")).Equal("\x1b[48;2;33;66;99mABC\x1b[49m")
+			g.Assert(Colorize("ABC", "bgRGB:33,123,3000")).Equal("ABC")
+		})
+
+		g.It("Supports ANSI 16m HEX colors, ignoring invalid colors", func() {
+			g.Assert(Colorize("ABC", "hex:F0d030")).Equal("\x1b[38;2;240;208;48mABC\x1b[39m")
+			g.Assert(Colorize("ABC", "bgHEX:0099FF")).Equal("\x1b[48;2;0;153;255mABC\x1b[49m")
+			g.Assert(Colorize("ABC", "bgHEX:0099GG")).Equal("ABC")
+		})
+	})
+
+	g.Describe("TestColorizeTemplate", func() {
+		g.It("Applies known styles and closes them in the right order", func() {
+			g.Assert(ColorizeTemplate("{red}ABC{green}CDE{-}EFG{-}HIJ")).Equal("\x1b[31mABC\x1b[32mCDE\x1b[39m\x1b[31mEFG\x1b[39mHIJ\x1b[0m")
+		})
+
+		g.It("Ignores unknown styles", func() {
+			g.Assert(ColorizeTemplate("{red}ABC{yolla}CDE{-}EFG{-}HIJ")).Equal("\x1b[31mABCCDE\x1b[39mEFGHIJ\x1b[0m")
+		})
+
+		g.It("Ignores unbalanced parenthesis", func() {
+			g.Assert(ColorizeTemplate("{red}}ABC{-}")).Equal("\x1b[31m}ABC\x1b[39m\x1b[0m")
+		})
+
+		g.It("Ignores unbalanced tags", func() {
+			g.Assert(ColorizeTemplate("{red}ABC")).Equal("\x1b[31mABC\x1b[0m")
+		})
+
+		g.It("Double curly braces are respected", func() {
+			g.Assert(ColorizeTemplate("{{red}")).Equal("{red}")
+		})
+
+		g.It("Closing tag ignores further specs", func() {
+			g.Assert(ColorizeTemplate("{red}ABC{green}CDE{- yellow}EFG{-}HIJ")).Equal("\x1b[31mABC\x1b[32mCDE\x1b[39m\x1b[31mEFG\x1b[39mHIJ\x1b[0m")
+		})
+
+		g.It("Reset tag cleans the stack", func() {
+			g.Assert(ColorizeTemplate("{red}ABC{green}CDE{reset red}EFG{-}HIJ")).Equal("\x1b[31mABC\x1b[32mCDEEFGHIJ\x1b[0m")
+		})
+
+		g.It("Supports ANSI, RGB and HEX colors", func() {
+			g.Assert(ColorizeTemplate("{ANSI:5,0,0}ABC{RGB:0,255,0}CDE{bgHEX:#0000FF}EFG")).Equal("\x1b[38;5;196mABC\x1b[38;2;0;255;0mCDE\x1b[48;2;0;0;255mEFG\x1b[0m")
+		})
+	})
+
+	g.Describe("TestColorizeTemplate", func() {
+		g.It("Allow to define custom styles, supported both by Colorize and ColorizeTemplate", func() {
+			g.Assert(Colorize("ABC", "customRed@@")).Equal("ABC")
+			g.Assert(ColorizeTemplate("{customRed@@ green}ABC{-}")).Equal("\x1b[32mABC\x1b[39m\x1b[0m")
+
+			AddCustomStyle("customRed@@", "red", "underline")
+
+			g.Assert(Colorize("ABC", "customRed@@")).Equal("\x1b[31m\x1b[4mABC\x1b[24m\x1b[39m")
+			g.Assert(ColorizeTemplate("{customRed@@ green}ABC{-}")).Equal("\x1b[31m\x1b[4m\x1b[32mABC\x1b[24m\x1b[39m\x1b[39m\x1b[0m")
+
+			DeleteCustomStyles("customRed@@")
+
+			g.Assert(Colorize("ABC", "customRed@@")).Equal("ABC")
+			g.Assert(ColorizeTemplate("{customRed@@ green}ABC{-}")).Equal("\x1b[32mABC\x1b[39m\x1b[0m")
+		})
+
+		g.It("Should reject custom styles name which contain spaces or curly brace", func() {
+			for _, s := range []string{"{invalid", "invalid}", "no spaces"} {
+				g.Assert(AddCustomStyle(s, "red")).Equal(errors.New("The custom style name should not contain spaces or curly braces"))
+			}
+		})
+	})
+}
